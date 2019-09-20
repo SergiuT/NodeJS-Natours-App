20e15a87a2a1e425cdd952b7775be5bc326c9dcb
SergiuT <sergiu_sergiu855@yahoo.com>
2019-09-20 13:38:54 +0300

    working on error handling

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`));
-
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -21,5 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,9 +4,15 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/').get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour);
+router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+// router.param('id', tourController.checkID);
+
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
 router.route('/:id').get(tourController.getOneTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
