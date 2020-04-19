1339f04269a73ed2f32c742db4586a9d55d3f4e2
TONT SERGIU <49995212+SergiuT@users.noreply.github.com>
2020-04-19 10:32:17 +0300

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
